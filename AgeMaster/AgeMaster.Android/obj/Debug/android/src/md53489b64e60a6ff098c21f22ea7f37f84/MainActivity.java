4fddab19ae8f5cb2dc482bdd6b09b8fb9f57c897
Calvin Berndt <calvinwberndtjr@gmail.com>
2018-09-19 19:03:26 -0500

    Add AgeMaster and AddMaster

---
+++ AgeMaster/AgeMaster.Android/obj/Debug/android/src/md53489b64e60a6ff098c21f22ea7f37f84/MainActivity.java
@@ -0,0 +1,47 @@
+package md53489b64e60a6ff098c21f22ea7f37f84;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("AgeMaster.Droid.MainActivity, AgeMaster.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("AgeMaster.Droid.MainActivity, AgeMaster.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ AgeMaster/AgeMaster/AgeMaster.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AgeMaster
+{
+    public partial class AgeMaster : ContentPage
+    {
+        public AgeMaster()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            string monthString = monthEntry.Text;
+            string dayString = dayEntry.Text;
+            string yearString = yearEntry.Text;
+
+            int month, day, year;
+
+            if (int.TryParse(monthString, out month))
+            {
+                //yayaya!
+                if (int.TryParse(dayString, out day))
+                {
+                    //noo!
+                }
+                else{
+                    //noo!
+                }
+                    if (int.TryParse(yearString, out year))
+                    {
+                    //yess!
+                    }
+                    else
+                    {
+
+                    }
+            }
+            
+            month = int.Parse(monthString);
+            day = int.Parse(dayString);
+            year = int.Parse(yearString);
+
+            DateTime birthDate = new DateTime(year, month, day);
+
+            TimeSpan age = DateTime.Now - birthDate;
+
+            int daysOfLife = age.Days;
+
+            int yearsOfLife = daysOfLife / 365;
+
+            AgeLabel.Text = yearsOfLife.ToString();
+
+        }
+    }
+}
