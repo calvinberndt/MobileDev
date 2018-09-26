80d87eb7ee12effa17987f96e8c42a143eecaa1c
Calvin Berndt <calvinwberndtjr@gmail.com>
2018-09-26 17:12:53 -0500

    TimerPro Added

---
+++ prjTimerPro/prjTimerPro/Custom/cTimerLogic.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace prjTimerPro.Custom
+{
+    public class cTimerLogic
+    {
+        private int _Sec;
+        private int _Min;
+        private int _Hours;
+
+        public cTimerLogic()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            _Sec = 0;
+            _Min = 0;
+            _Hours = 0;
+        }
+
+        public void SecTickCounter()
+        {
+            _Sec++;
+            if (_Sec == 60)
+            {
+                _Min++;
+                _Sec = 0;
+            }
+            if (_Min==60)
+            {
+                _Hours++;
+                _Min = 0;
+            }
+        }
+
+        public string GetFormatedString()
+        {
+            return _Hours.ToString().PadLeft(2, '0') + ":" +
+                         _Min.ToString().PadLeft(2, '0') + ":" +
+                         _Sec.ToString().PadLeft(2, '0');
+        }
+    }
+}
---
+++ prjTimerPro/prjTimerPro/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using prjTimerPro.Custom;
+using System.Threading;
+
+namespace prjTimerPro
+
+{
+    public partial class MainPage : ContentPage
+    {
+
+        cTimerLogic otimerlogic;
+        bool isRunning; 
+
+        public MainPage()
+        {
+            InitializeComponent();
+            otimerlogic = new cTimerLogic();
+        }
+
+        void Start_Clicked(object sender, System.EventArgs e)
+        {
+            btnStop.IsEnabled = true;
+            btnStart.IsEnabled = false;
+            isRunning = true;
+
+            Device.StartTimer(TimeSpan.FromSeconds(1),() => {
+                otimerlogic.SecTickCounter();
+                lblDisplay.Text = otimerlogic.GetFormatedString();
+                return isRunning;
+            });
+        }
+
+        void Stop_Clicked(object sender, System.EventArgs e)
+        {
+            btnStop.IsEnabled = false;
+            btnStart.IsEnabled = true;
+            isRunning = false;
+        }
+
+        void Reset_Clicked(object sender, System.EventArgs e)
+        {
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            btnStart.IsEnabled = true;
+            btnStop.IsEnabled = false;
+            lblDisplay.Text = "00:00:00";
+        }
+    }
+}
